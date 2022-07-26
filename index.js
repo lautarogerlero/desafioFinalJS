a0795e61cd8cbc36c4c7e7b93ba72ad49d9c5455
lautarogerlero <lautarogerlero@gmail.com>
2022-07-26 16:47:40 -0300

    Agregando eventos

--- index.js
+++ index.js
@@ -7,110 +7,99 @@
 // Finalmente el usuario debe elegir un metodo de pago válido y en base a eso se le muestra el valor que pagará
 
 // objetos del DOM
-const carritoDeCompras = document.querySelector("#carrito");
-const precio = document.querySelector("#precio");
+const cantidades = document.querySelector("#cantidades");
+const pago = document.querySelector("#metodo-pago");
+const mostrarPrecioFinal = document.querySelector("#valorFinal");
+const carrito = document.querySelector("#carrito");
+const botonVerCarrito = document.querySelector("#verCarrito");
+const botonPago = document.querySelector("#elegirPago");
+const efectivo = document.querySelector("#efectivo");
+const unaCuota = document.querySelector("#unaCuota");
+const tresCuotas = document.querySelector("#tresCuotas");
+const seisCuotas = document.querySelector("#seisCuotas");
+const textoValorFinal = document.querySelector("#tituloValorFinal");
+
+let pantalones = document.querySelector("#comprar-pantalon");
+let buzos = document.querySelector("#comprar-buzo");
+let remeras = document.querySelector("#comprar-remera");
 
 // Array con la lista de productos
 const productos = ["pantalon", "buzo", "remera"];
 
-// variables
-let pantalones = 0;
-let buzos = 0;
-let remeras = 0;
-
-// constantes
+// precios
 const precioPantalon = 4000;
 const precioRemera = 2500;
 const precioBuzo = 6000;
 
-// ciclo while
-let terminar = false;
-while (terminar === false){
-    let producto = prompt("Escriba el producto que desea comprar: 'Pantalon', 'Buzo', 'Remera'")
-    // Si el producto ingresado está en la lista se ejecuta el primer if
-    if (productos.includes(producto.toLowerCase())){
-        if (producto.toLowerCase() === "pantalon"){
-            pantalones += 1
-        }
-        else if (producto.toLowerCase() === "buzo"){
-            buzos += 1
-        }
-        else if (producto.toLowerCase() === "remera"){
-            remeras += 1
-        }
-    }
-    else{
-        alert("Ingrese un producto valido")
-    }
-    let pregunta = prompt("Escriba NO si no desea comprar nada más")
-    if (pregunta.toUpperCase() === "NO"){
-        terminar = true
-    }
-}
-
-// DOM
-carritoDeCompras.innerText = `${pantalones} pantalones
-                              ${buzos} buzos
-                              ${remeras} remeras`
 
 // funcion para calcular el precio total de cada producto
 function calcularTotal(producto, precio){
     return producto * precio
 }
 
-totalPantalones = calcularTotal(pantalones, precioPantalon);
-totalRemeras = calcularTotal(remeras, precioRemera);
-totalBuzos = calcularTotal(buzos, precioBuzo);
+// Apretar el boton para ir al carrito
+botonVerCarrito.onclick = () => {
+    // Obtener la cantidad de cada producto y calcular el precio
+    totalPantalones = calcularTotal(pantalones.value, precioPantalon);
+    totalRemeras = calcularTotal(remeras.value, precioRemera);
+    totalBuzos = calcularTotal(buzos.value, precioBuzo);
 
-// precio final
-valorFinal = totalPantalones + totalBuzos + totalRemeras;
+    // Calcular precio final
+    valorFinal = totalPantalones + totalBuzos + totalRemeras;
 
-alert(`El valor final a pagar es $${valorFinal}.
-        Puede pagar en 1, 3 o 6 cuotas sin interes.
-        Con traferencia bancaria tiene un 10% de descuento`);
-// console.log para que quede en consola tambien
-console.log(`El valor final a pagar es $${valorFinal}.
-        Puede pagar en 1, 3 o 6 cuotas sin interes.
-        Con transferencia bancaria tiene un 10% de descuento`);
-
-const mediosDePago = [
-    {
-        transferencia: valorFinal - valorFinal * 0.1,
-        credito1Cuota: valorFinal,
-        credito3Cuotas: valorFinal / 3,
-        credito6Cuotas: valorFinal / 6
-    }
-]
-
-let cerrar = false;
-while (cerrar === false){
-    let pago = prompt("Elija un método de pago (A, B, C o D). A: Transferencia, B: Crédito en 1 cuota, C: Crédito en 3 cuotas, D: Crédito en 6 cuotas")
-    if (pago.toUpperCase() === "A"){
-        precio.innerText = `El valor final con el descuento es $${mediosDePago[0].transferencia}`
-        // console.log para que quede en consola tambien
-        console.log(`El valor final con el descuento es $${mediosDePago[0].transferencia}`)
-        break
-    }
-    else if (pago.toUpperCase() === "B"){
-        precio.innerText = `El pago será en 1 cuota de $${mediosDePago[0].credito1Cuota}`
-        // console.log para que quede en consola tambien
-        console.log(`El pago será en 1 cuota de $${mediosDePago[0].credito1Cuota}`)
-        break
-    }
-    else if (pago.toUpperCase() === "C"){
-        precio.innerText = `El pago será en 3 cuotas de $${mediosDePago[0].credito3Cuotas}`
-        // console.log para que quede en consola tambien
-        console.log(`El pago será en 3 cuotas de $${mediosDePago[0].credito3Cuotas}`)
-        break
-    }
-    else if (pago.toUpperCase() === "D"){
-        precio.innerText = `El pago será en 6 cuotas de $${mediosDePago[0].credito6Cuotas}`
-        // console.log para que quede en consola tambien
-        console.log(`El pago será en 6 cuotas de $${mediosDePago[0].credito6Cuotas}`)
-        break
+    // Mostrar las cantidades y el precio de cada producto en el carrito
+    cantidades.innerText = `${remeras.value} remeras: $${totalRemeras}
+                            ${buzos.value} buzos: $${totalBuzos}
+                            ${pantalones.value} pantalones: $${totalPantalones}
+                            Valor final: $${valorFinal}`
+    
+    // Hacer visible el carrito
+    carrito.classList.toggle("ver");
+    if (carrito.classList.contains("ver")){
+        botonVerCarrito.textContent = "Cerrar carrito";
     }
     else {
-        alert("Elija una opción válida")
+        botonVerCarrito.textContent = "Ver carrito";
     }
+    
+    // Array para calcular el vlaor final dependiendo del método de pago
+    const mediosDePago = [
+        {
+            transferencia: valorFinal - valorFinal * 0.1,
+            credito1Cuota: valorFinal,
+            credito3Cuotas: valorFinal / 3,
+            credito6Cuotas: valorFinal / 6
+        }
+    ]
+
+    // Elegir método de pago 
+    // Hacer visible la sección 
+    botonPago.onclick = () => {
+        pago.classList.toggle("ver");
+    }
+
+    // Dependiendo del método elegido se muestra un texto con el valor a pagar
+    efectivo.onclick = () => {
+        mostrarPrecioFinal.classList.toggle("ver");
+        textoValorFinal.innerText = `El valor a pagar en efectivo es $${mediosDePago[0].transferencia}`;
+    }
+    unaCuota.onclick = () => {
+        mostrarPrecioFinal.classList.toggle("ver");
+        textoValorFinal.innerText = `El valor a pagar en 1 cuota es $${mediosDePago[0].credito1Cuota}`;
+    }
+    tresCuotas.onclick = () => {
+        mostrarPrecioFinal.classList.toggle("ver");
+        textoValorFinal.innerText = `El valor a pagar en 3 cuotas es $${mediosDePago[0].credito3Cuotas}`;
+    }
+    seisCuotas.onclick = () => {
+        mostrarPrecioFinal.classList.toggle("ver");
+        textoValorFinal.innerText = `El valor a pagar en 6 cuotas es $${mediosDePago[0].credito6Cuotas}`;
+    }
+
 }
+
+
+
+
+
 
